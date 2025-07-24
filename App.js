1c6d623d8a0fadd90c06df9d8d19c13584a3977a
RkGreenPhire <rakesh.kumar@greenphire.com>
2025-07-24 13:49:28 +0530

    JSX introduction in code

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", {}, "Hello World From React !");
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "This is h1 from React.createElement"
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+// Using JSX
+
+const JSXHeading = <h1 id="heading"> This is h1 from JSX 🚀🚀</h1>;
+
+root.render(JSXHeading);
