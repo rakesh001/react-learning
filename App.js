8c985cf4ac1d2d032ad95ac3ff59cd762f35b952
RkGreenPhire <rakesh.kumar@greenphire.com>
2025-07-24 17:00:42 +0530

    header bar as assignment from 3rd episode

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "This is h1 from React.createElement"
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
 // Using JSX
-
-const JSXHeading = <h1 id="heading"> This is h1 from JSX 🚀🚀</h1>;
-
-root.render(JSXHeading);
+const Logo = () => (
+  <div className="logo-wrapper">
+    <img src="https://picsum.photos/200/300" alt="Random" />
+  </div>
+);
+const SearchBar = () => (
+  <div className="search-wrapper">
+    <input type="text" placeholder="Search..."></input>
+  </div>
+);
+const UserIcon = () => (
+  <div className="user-icon-wrapper">
+    <img src="https://picsum.photos/200/300" alt="Random" />
+  </div>
+);
+const Heading = () => (
+  <div id="header">
+    {Logo()}
+    <SearchBar />
+    <UserIcon></UserIcon>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Heading />);
